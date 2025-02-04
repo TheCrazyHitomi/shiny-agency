771fddcfdafa2370c2074f4f73598ae728dc06be
TheCrazyHitomi <amandine-tomas@hotmail.fr>
2025-02-04 17:29:33 +0100

    add propsTypes to Card component

--- src/pages/Freelance/Freelances.jsx
+++ src/pages/Freelance/Freelances.jsx
@@ -1,7 +1,41 @@
+import DefaultPicture from '../../assets/default-picture.jpg';
+import Card from '../Card';
+
+const freelanceProfiles = [
+    {
+        name : "John Doe",
+        jobTitle: "DevOps",
+        picture : DefaultPicture,
+    },
+    {
+        name : "Johnny Biche",
+        jobTitle: "Developpeur Frontend",
+        picture : DefaultPicture,
+    },
+    {
+        name : "Jane Doe",
+        jobTitle: "Developpeuse Fullstack",
+        picture : DefaultPicture,
+    }, {
+        name : "Josette Biche",
+        jobTitle: "Developpeuse Backend",
+        picture : DefaultPicture,
+    }
+]
+
+
 const Freelances = () => {
     return (
         <div>
-            <h1>Freelances</h1>
+            <h1>👩·💻👨·💻 Freelances 💻👨·💻·👩</h1>
+            {freelanceProfiles.map((profile, index) => (
+                <Card 
+                    key={`${profile.name}-${index}`}
+                    label={profile.jobTitle}
+                    picture={profile.picture}
+                    title={profile.name}
+                />
+            ))}   
         </div>
     )
 }
